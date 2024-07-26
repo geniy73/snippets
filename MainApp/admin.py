92ef82f1663cbddf2b4f4ef8559c7b9c09cc8625
Alexey <geniy73@gmail.com>
2024-07-26 15:13:56 +0300

    admin + second var

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from MainApp.models import Snippet
 
 # Register your models here.
-admin.site.register(Snippet)
+# 1 variant
+#admin.site.register(Snippet)
+
+# 2 variant
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ["name", "lang", "code", "creation_date"]
+    list_filter = ["lang", "creation_date"]
+    ordering = ["creation_date"]
+    search_fields = ["name", "code"]
--- MainApp/models.py
+++ MainApp/models.py
@@ -12,11 +12,14 @@ LANGS = (
 
 class Snippet(models.Model):
     name = models.CharField(max_length=100)
-    lang = models.CharField(max_length=30, choices=LANGS)
+    lang = models.CharField(max_length=30, choices=LANGS, verbose_name="Язык")
     code = models.TextField(max_length=5000)
-    creation_date = models.DateTimeField(auto_now=True)
+    creation_date = models.DateTimeField(auto_now=True, verbose_name="Дата создания")
     user = models.ForeignKey(to=User, on_delete=models.CASCADE,blank=True, null=True)
     public = models.BooleanField(default=True) #True = public, False = private
 
     def __repr(self) -> str:
         return f'Snippet({self.name})'
+    
+    def __str__(self) -> str:
+        return f'{self.name}'
